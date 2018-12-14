3734820ed1ad48a0a048dffcd9857b0261092e22
prudhvi.kumar <prudhvi.kumar@i-exceed.com>
2018-12-14 18:15:02 +0530

    All

---
+++ scripts/navlinks.js
@@ -0,0 +1,27 @@
+function resume() {
+    window.open("./files/prudhviDevOps.pdf"); 
+}
+function github() {
+    window.open("https://github.com/prudhvikumar789");
+}
+function locateme() {
+    window.open("http://maps.google.com/maps?q=13.734907,79.153104");
+}
+function linkedin() {
+    window.open("https://www.linkedin.com/in/prudhvikumarmadithati/");
+}
+function gplus() {
+    window.open("http://google.com/+prudhvikumarmadithati");
+}
+function messenger() {
+    window.open("http://m.me/prudhvikumarreddy");
+}
+function twitter() {
+    window.open("https://twitter.com/iamprudhvikumar");
+}
+function skype() {
+    window.open("skype:prudhvikumarcse?chat");
+}
+function instagram() {
+    window.open("https://instagram.com/prudhvikumarreddy");
+}
